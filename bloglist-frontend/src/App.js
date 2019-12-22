2d4f08c1894dcf63bf3bf8e3e8f7d8b42c069869
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2019-12-22 12:43:18 +0200

    done Ex. 5.1: bloglist frontend, step1

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Blog from './components/Blog';
+import loginService from './services/login';
+import blogsService from './services/blogs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogsService
+      .getAll()
+      .then(blogs => setBlogs(blogs))
+  }, [])
+
+  const handleLogin = async e => {
+    e.preventDefault()
+    
+    try {
+      const user = await loginService.login({ username, password })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      console.log(error)      
+    }
+    
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>
+            Username
+            <input 
+              type="text"
+              name="username"
+              value={username}
+              onChange={({target}) => setUsername(target.value)}
+            />
+          </label>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <label>
+            Password
+            <input 
+              type="password"
+              name="password"
+              value={password}
+              onChange={({target}) => setPassword(target.value)}
+            />
+          </label>
+        </p>
+        <button type="submit">Log in</button>
+      </form>
     </div>
-  );
+  )
+
+  const blogRows = () => (
+    <div>
+      <h2>Blogs</h2>
+      <p>{user.name} logged in</p>
+      {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+    </div>
+  )
+
+  return (
+    <div>
+      { user === null ? loginForm() : blogRows() }
+    </div>
+  )
 }
 
 export default App;
